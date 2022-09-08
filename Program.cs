d1e6b771c364681dbeff46841847d6ee1951b2dd
Vydmintas Brokevičius <vydmintas@theimpactbrands.com>
2022-09-08 13:27:40 +0300

    Created test

--- Program.cs
+++ Program.cs
@@ -10,8 +10,9 @@ namespace HWork
         static void Main(string[] args)
         {
             IMeasurementsWorker measurementsWorker = new MeasurementsWorker();
+            InputsData inputsData = new InputsData();
 
-            var sampledList = measurementsWorker.SampleMeasurements(DateTime.Parse("2017 - 01 - 03T10: 02:00"), CreateInputs());
+            var sampledList = measurementsWorker.SampleMeasurements(DateTime.Parse("2017 - 01 - 03T10: 02:00"), inputsData.CreateInputs());
 
             ShowResults(sampledList);
         }
@@ -20,56 +21,6 @@ namespace HWork
         {
             foreach (var item in measurementsList)
                 Console.WriteLine(item.MeasurementTime + ", " + item.Type.ToString() + ", " + item.MeasurementValue);
-        }
-
-        private static List<Measurement> CreateInputs()
-        {
-            List<Measurement> measurementsList = new List<Measurement>();
-
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 04:45"),
-                Type = MeasurementType.TEMP,
-                MeasurementValue = 35.79
-            });
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 01:18"),
-                Type = MeasurementType.SPO2,
-                MeasurementValue = 98.78
-            });
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 09:07"),
-                Type = MeasurementType.TEMP,
-                MeasurementValue = 35.01
-            });
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 03:34"),
-                Type = MeasurementType.SPO2,
-                MeasurementValue = 96.49
-            });
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 02:01"),
-                Type = MeasurementType.TEMP,
-                MeasurementValue = 35.82
-            });
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 05:00"),
-                Type = MeasurementType.SPO2,
-                MeasurementValue = 97.17
-            });
-            measurementsList.Add(new Measurement()
-            {
-                MeasurementTime = DateTime.Parse("2017 - 01 - 03T10: 05:01"),
-                Type = MeasurementType.SPO2,
-                MeasurementValue = 95.08
-            });
-
-            return measurementsList;
         }
     }
 }
