824bbd35f0a510e3f791393fc8340ec9eac68243
vydmintas <66644835+vydmintas@users.noreply.github.com>
2022-09-08 17:24:29 +0300

    Upload project

---
+++ HWork/HWork.Tests/MeasurementsWorkerTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using HWork.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HWork.Tests
+{
+    [TestFixture]
+    public class MeasurementsWorkerTests
+    {
+        [Test]
+        public void SampleMeasurements_WithProvidedData_GiveExpectedResults()
+        {
+            //ARRANGE
+
+            var inputData = new InputsData();
+            var measurementsWorker = new MeasurementsWorker();
+            var expectedDataList = new List<Measurement>()
+            {
+                new Measurement(new DateTime(2017,01,03,10,05,00), MeasurementType.TEMP, 35.79),
+                new Measurement(new DateTime(2017,01,03,10,10,00), MeasurementType.TEMP, 35.01),
+                new Measurement(new DateTime(2017,01,03,10,05,00), MeasurementType.SPO2, 97.17),
+                new Measurement(new DateTime(2017,01,03,10,10,00), MeasurementType.SPO2, 95.08)
+            };
+
+            var expectedStringList = expectedDataList.Select(x => x.Stringify()).ToList();
+
+            //ACT
+
+            var results = measurementsWorker.SampleMeasurements(DateTime.Parse("2017 - 01 - 03T10: 02:00"), inputData.CreateInputs());
+
+            //ASSERT
+            results.Count.Should().Be(expectedDataList.Count);
+
+            results.Should().AllSatisfy(x =>
+            {
+                expectedStringList.Contains(x.Stringify());
+            });
+        }
+    }
+}
---
+++ HWork/HWork/Models/MeasurementsByTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWork.Models
+{
+    public class MeasurementsByTime
+    {
+        public DateTime TimeStamp { get; set; }
+        public List<Measurement> Measurements { get; set; }
+    }
+}
